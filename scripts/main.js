91eaa7f200f2fc3f41332bd25de5e1308494604c
m4nlikezu <l1l_h1tm4n@hotmail.co.uk>
2017-02-18 23:24:38 +0000

    18-02-2017 11:24pm

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,5 @@
 //mobile menu //
 
-// toggleMenu();
-// window.onresize = function(){
-//   toggleMenu();
-// }
-//
-// function toggleMenu() {
-//     if (window.innerWidth < 800){
-//       document.getElementById("mobileNav").style.display = "none";
-//     }
-//     else {
-//       document.getElementById("mobileNav").style.display = "inline-block";
-//     }
-// }
-
-
 function toggleMenu() {
   var x = document.getElementById('mobileNav');
   if (x.style.display === "inline-block") {
