db918170b6b2af6aaa1a11f0d26037a36c127bcb
m4nlikezu <l1l_h1tm4n@hotmail.co.uk>
2017-03-23 23:16:11 +0000

    SEO improvement

--- contactForm.php
+++ contactForm.php
@@ -5,14 +5,14 @@ $contactEmail = $_POST['email'];
 $contactQuery = $_POST['query'];
 //variables for the mail function
 $to = "contact@zuhayr-b.co.uk";
-$subject = "My Website Contacted by -  $contactName";
-$mailMessage = "You were contacted by $contactName Their Email: $contactEmail\r\n Their query: $contactQuery\n";
+$subject = "My Website Contacted by - $contactName";
+$mailMessage = "You were contacted by $contactName\r\n\r\nTheir Email:    $contactEmail\r\nTheir query:\r\n\r\n$contactQuery\n";
 $mailheader = "From: $contactEmail \r\n";
-$subject2 = "Copy of your form submission"; //Sender recieves.
-$message2 = "Hi thanks for getting in touch! I shall be in contact with you shortly.";
+$subject2 = "Automatic Reply - Zuhayr | Freelance Web Designer "; //Sender recieves.
+$message2 = "Hi $contactName,\r\n\r\nYour email has been received.\r\n\r\nYour Message:\n$contactQuery\r\n\r\nThanks for getting in touch, I aim to reply to all emails within 24 hours.";
+$mailheader2 = "From: $to \r\n";
 mail($to, $subject, $mailMessage, $mailheader);
-// mail($contactEmail, $subject2, $message2, $mailheader2); // reciepient recieves email confirmation
+mail($contactEmail, $subject2, $message2, $mailheader2); // recipient receives email confirmation
 header('Location: http://www.zuhayr-b.co.uk/');
 exit;
-
 ?>
